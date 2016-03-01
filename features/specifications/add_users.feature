ff0a13849f41321bb7c7a49c7640ee312574050d
Biro Biro <thiagomarquessp@gmail.com>
2016-03-01 00:40:51 -0300

    Add users scenario.

---
+++ features/specifications/add_users.feature
@@ -0,0 +1,9 @@
+Feature: Customer Registration success
+
+  Scenario: As a user, I want register one customer
+
+    Given I click add users
+    When I click Convidar Usuario
+    And I fill out the add fields for user
+    And I send invitation
+    Then the user successfully invited
---
+++ features/step_definitions/add_users.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+#!/usr/bin/env ruby
+
+Given(/^I click add users$/) do
+  click_link 'Adicione usuários'
+  sleep 05
+end
+
+When(/^I click Convidar Usuario$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill out the add fields for user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I send invitation$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user successfully invited$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
