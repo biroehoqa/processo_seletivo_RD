d59ba99997667cd68853c1fdc848b5f21ee82705
Biro Biro <thiagomarquessp@gmail.com>
2016-03-01 21:58:50 -0300

    Modify time out.

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@ require "cpf_faker"
 require "rspec"
 
 Capybara.default_driver = :selenium
-Capybara.default_max_wait_time = 60
+Capybara.default_max_wait_time = 10
 
 Capybara::Webkit.configure do |config|
   config.allow_url("https://www.rdstation.com.br/cadastro")
